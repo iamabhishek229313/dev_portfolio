679ba55bb825cc0387cf29f8d294d69211ee03f7
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-06-10 12:29:18 +0530

    Made something worth looking .

--- lib/presentation/landing_page/landing_page.dart
+++ lib/presentation/landing_page/landing_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dev_portfolio/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -41,41 +43,132 @@ class _LandingPageState extends State<LandingPage> {
           ),
         ],
       ),
-      body: Row(
-        children: [
-          new Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-                children: ["A", "b", "h", "i", "s", "h", "e", "k"]
-                    .map((e) => TransalteContainer(
-                            child: Text(
-                          e,
-                          style: TextStyle(
-                              fontSize: 32.0,
-                              fontWeight: FontWeight.w700,
-                              fontFamily: 'BioRhyme'),
-                        )))
-                    .toList()),
+      body: ListView(physics: BouncingScrollPhysics(), children: [
+        Container(
+          height: screenHeight * 5, // Height of entire Container .
+          child: Row(
+            children: [
+              Expanded(
+                // The main Carousel .
+                flex: 12,
+                child: Column(
+                  children: [
+                    ScreenHeightWidget(
+                      child: Row(
+                        children: [
+                          new Expanded(
+                            flex: 2,
+                              child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 50),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Wrap(
+                                      runAlignment: WrapAlignment.center,
+                                      children: [
+                                        "I'm ",
+                                        "an ",
+                                        "app ",
+                                        "developer ",
+                                        "and ",
+                                        "a ",
+                                        "freelancer."
+                                      ]
+                                          .map((e) => EachString(
+                                                text: e,
+                                              ))
+                                          .toList()),
+                                ),
+                                Expanded(
+                                  child: Wrap(
+                                    runAlignment: WrapAlignment.center,
+                                    children: [
+                                      Text(
+                                        "```\nHi! I'm Abhishek Kumar, a Flutter Developer living in Bhubaneswar, India.A flutter/Dart enthusiast who loves contributing to open source community.\n\n```",
+                                        style: TextStyle(
+                                            fontFamily: 'FiraCode',
+                                            fontSize: 24.0,
+                                            fontWeight: FontWeight.w200,
+                                            color: Colors.blueGrey.shade50),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )),
+                          new Expanded(
+                              child: Center(
+                            child: Image(
+                                height: screenHeight,
+                                width: screenWidth * 0.45,
+                                fit: BoxFit.contain,
+                                image: AssetImage(
+                                    'assets/images/Portfolio_image_final.jpg')),
+                          )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              new Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: List.generate(
+                        (screenHeight * 4 / 40.0).floor(),
+                        (index) => Text(
+                              "|",
+                              style: TextStyle(
+                                  fontSize: 40.0, fontWeight: FontWeight.bold),
+                            )),
+                  ))
+            ],
           ),
-          new Expanded(
-              child: Center(
-            child: Image(
-                height: screenHeight,
-                width: screenWidth * 0.45,
-                fit: BoxFit.contain,
-                image: AssetImage('assets/images/Portfolio_image_final.jpg')),
-          ))
-        ],
-      ),
+        ),
+      ]),
     );
+  }
+}
+
+class EachString extends StatefulWidget {
+  final String text;
+
+  const EachString({Key key, @required this.text}) : super(key: key);
+
+  @override
+  _EachStringState createState() => _EachStringState();
+}
+
+class _EachStringState extends State<EachString> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(
+          widget.text.length,
+          (index) => TransalteContainer(
+            child: Text(
+              widget.text[index],
+              style: TextStyle(
+                  color: widget.text == "I'm " ? Colors.red : Colors.grey,
+                  fontSize: 44.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'BioRhyme'),
+            ),
+          ),
+        ));
   }
 }
 
 class TransalteContainer extends StatefulWidget {
   final Widget child;
 
-  const TransalteContainer({Key key, this.child}) : super(key: key);
+  const TransalteContainer({Key key, @required this.child}) : super(key: key);
 
   @override
   _TransalteContainerState createState() => _TransalteContainerState();
@@ -103,5 +196,19 @@ class _TransalteContainerState extends State<TransalteContainer> {
     setState(() {
       _hovering = val;
     });
+  }
+}
+
+class ScreenHeightWidget extends StatelessWidget {
+  final Widget child;
+
+  const ScreenHeightWidget({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: child,
+    );
   }
 }
